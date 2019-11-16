b3a2fd53045f7f56203cd9eda7e632371d3514af
Parras <Parras@LAPTOP-371PRH8M>
2019-11-16 18:15:44 +0100

    Añadiendo la funcion de crear, editar y borrar banners

--- src/main/java/acme/features/administrator/nonCommercialBanner/AdministratorNonCommercialBannerController.java
+++ src/main/java/acme/features/administrator/nonCommercialBanner/AdministratorNonCommercialBannerController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import acme.entities.banners.CommercialBanner;
 import acme.entities.banners.NonCommercialBanner;
 import acme.framework.components.BasicCommand;
 import acme.framework.controllers.AbstractController;
@@ -18,16 +17,28 @@ import acme.framework.entities.Administrator;
 public class AdministratorNonCommercialBannerController extends AbstractController<Administrator, NonCommercialBanner> {
 
 	@Autowired
-	private AdministratorNonCommercialBannerListService	listService;
+	private AdministratorNonCommercialBannerListService		listService;
 
 	@Autowired
-	private AdministratorNonCommercialBannerShowService	showService;
+	private AdministratorNonCommercialBannerShowService		showService;
+
+	@Autowired
+	private AdministratorNonCommercialBannerCreateService	createService;
+
+	@Autowired
+	private AdministratorNonCommercialBannerUpdateService	updateService;
+
+	@Autowired
+	private AdministratorNonCommercialBannerDeleteService	deleteService;
 
 
 	@PostConstruct
 	private void initialise() {
 		super.addBasicCommand(BasicCommand.LIST, this.listService);
 		super.addBasicCommand(BasicCommand.SHOW, this.showService);
+		super.addBasicCommand(BasicCommand.CREATE, this.createService);
+		super.addBasicCommand(BasicCommand.UPDATE, this.updateService);
+		super.addBasicCommand(BasicCommand.DELETE, this.deleteService);
 	}
 
 }
